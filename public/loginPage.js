6f1735ae719073826feb5599a7fceaa7abdb854b
Dima Neto <kuznetsovdv2013@gmail.com>
2025-02-14 16:17:10 +0300

    Первая версия loginPage

---
+++ public/loginPage.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const { response } = require("express");
+
+const userForm = new UserForm();
+userForm.loginFormCallback = function(data) {
+    console.log(data);
+    ApiConnector.login(data, (response) =>
+    console.log(response));
+    if (response.success) {
+        location.reload();
+    } else {
+        userForm.setLoginErrorMessage(message)
+    }
+}
+userForm.registerFormCallback = function (data) {
+    console.log(data);
+    ApiConnector.login(data, (response) => 
+    console.log(response));
+    if (response.success) {
+        location.reload();
+    } else {
+        userForm.setRegisterErrorMessage(message)
+    }
+}
+
+
